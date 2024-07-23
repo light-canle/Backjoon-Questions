fc10a5fe94e5f98a2b2c3ead0dd10a2bf0d3c544
Jaemin Lim <87157622+light-canle@users.noreply.github.com>
2024-07-23 19:39:20 +0900

    Create p1448.cs

---
+++ p1448.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        StreamReader sr = new (new BufferedStream(Console.OpenStandardInput()));
+        int N = int.Parse(sr.ReadLine());
+
+        List<int> list = new List<int>();
+        for (int i = 0; i < N; i++)
+        {
+            list.Add(int.Parse(sr.ReadLine()));
+        }
+
+        list.Sort();
+
+        int curIdx = list.Count - 3;
+
+        int max = -1;
+        while (curIdx >= 0)
+        {
+            if (CamMakeTriangle(list[curIdx + 0], list[curIdx + 1], list[curIdx + 2]))
+            {
+                int cur = list[curIdx + 0] + list[curIdx + 1] + list[curIdx + 2];
+                if (cur > max)
+                {
+                    max = cur;
+                    break;
+                }
+            }
+            curIdx--;
+        }
+        
+        Console.WriteLine(max);
+        sr.Close();
+    }
+
+    public static bool CamMakeTriangle(int a, int b, int c)
+
+    {
+        int max = Math.Max(a, Math.Max(b, c));
+        return max < a + b + c - max;
+    }
+}
