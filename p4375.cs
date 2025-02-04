1afb2e071b8cf74a5bba85daac53425c717f8d85
Jaemin Lim <87157622+light-canle@users.noreply.github.com>
2025-02-04 20:21:10 +0900

    Update p4375.cs
    주석 추가

--- p4375.cs
+++ p4375.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Numerics;
 
+// p4375 - 1 (S3)
+// #정수론 #완전 탐색
+// 2025.2.4 solved
+
 public class Program
 {
     public static void Main(string[] args)
@@ -14,10 +18,13 @@ public class Program
             }
             BigInteger n = BigInteger.Parse(line);
 
-            int digit = 0;
-            BigInteger cur = 0;
-            BigInteger toAdd = 1;
+            int digit = 0; // 현재 자릿수(1로만 이루어진 수의 자릿수)
+            BigInteger cur = 0; // 현재 수를 n으로 나눈 나머지
+            BigInteger toAdd = 1; // 더해갈 자릿수
 
+            // 1, 10, 100, ... 씩 더해가면서
+            // n으로 나누었을 때 나머지가 0이 되는 자릿수를 찾는다.
+            // (a+b) % r = ((a % r) + (b % r)) % r 임을 이용한다.
             do 
             {
                 cur += toAdd;
@@ -25,7 +32,7 @@ public class Program
                 toAdd *= 10;
                 digit++;
             }
-            while (cur > 0);
+            while (cur > 0); // 나머지가 0이 되면 종료
             Console.WriteLine(digit);
         }
     }
