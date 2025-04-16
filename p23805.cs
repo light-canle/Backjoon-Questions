157a893cb2ba8a6d9cbee55aa230a266e2ae79a7
Jaemin Lim <87157622+light-canle@users.noreply.github.com>
2025-04-16 20:33:11 +0900

    Update p23805.cs

--- p23805.cs
+++ p23805.cs
@@ -13,14 +13,18 @@ public class Program
         int n = int.Parse(Console.ReadLine());
 
         bool[,] arr = new bool[5 * n, 5 * n];
+        // 전체를 5x5부분으로 나눈다.
+        // 각 부분의 크기는 n×n이다.
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
             {
+                // 1, 3, 5번째 열은 @를 출력한다.
                 if (j % 2 == 0)
                 {
                     Fill(arr, i * n, j * n, n);
                 }
+                // 2번째 열은 1번 행에만, 4번째 열은 5번 행에만 @를 출력한다.
                 else if ((j == 1 && i == 0) || (j == 3 && i == 4))
                 {
                     Fill(arr, i * n, j * n, n);
@@ -39,7 +43,8 @@ public class Program
         sw.Close();
     }
 
-    public static void Fill(bool [,] arr, int i, int j, int size)
+    // i, j부터 size×size 크기로 true를 채운다.
+    public static void Fill(bool[,] arr, int i, int j, int size)
     {
         for (int y = i; y < i + size; y++)
         {
